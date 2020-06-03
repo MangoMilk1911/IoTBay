a4eec09d1e015725eeb927d40db612e1cd03c8ad
kevinguyen76 <62038441+kevinguyen76@users.noreply.github.com>
2020-06-03 14:29:21 +1000

    Changed name of PaymentServlet to PaymentInfoServlet

--- src/java/uts/isd/controller/PaymentInfoServlet.java
+++ src/java/uts/isd/controller/PaymentInfoServlet.java
@@ -6,7 +6,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-public class PaymentServlet extends HttpServlet {
+public class PaymentInfoServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
         //User clicks checkout
