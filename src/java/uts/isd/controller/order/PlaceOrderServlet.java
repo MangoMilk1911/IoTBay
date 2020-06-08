60836c279c7a886e134de3c3c7c95cccc4373d4e
LonJonn <leon.salsiccia@gmail.com>
2020-06-08 10:12:38 +1000

    Clean up

--- src/java/uts/isd/controller/order/PlaceOrderServlet.java
+++ src/java/uts/isd/controller/order/PlaceOrderServlet.java
@@ -3,10 +3,8 @@ package uts.isd.controller.order;
 import uts.isd.model.Customer;
 import uts.isd.model.Order;
 import uts.isd.model.OrderLineItem;
-import uts.isd.model.Product;
 import uts.isd.model.dao.DAOException;
 import uts.isd.model.dao.OrderDAO;
-import uts.isd.model.dao.ProductDAO;
 
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -26,15 +24,13 @@ public class PlaceOrderServlet extends HttpServlet {
         order.setCustomer(user);
         order.setOrderedProducts(cart);
         order.setShippingAddress(user.getAddress().getAddressLine1());
-        order.setTrackingID("123HEHEHERACKINGID");
 
         try {
             int newOrderID = OrderDAO.save(order);
 
-            LinkedList<Order> customerOrders = user.getOrders();
-            customerOrders.add(order);
+            order.setTrackingID("TRACK_ORD_#" + newOrderID);
 
-            user.setOrders(customerOrders);
+            user.getOrders().add(order);
 
             session.setAttribute("user", user);
             session.setAttribute("cart", new LinkedList<>());
@@ -48,15 +44,5 @@ public class PlaceOrderServlet extends HttpServlet {
             err.printStackTrace();
             response.sendRedirect("cart.jsp");
         }
-    }
-
-    private OrderLineItem productInCart(LinkedList<OrderLineItem> cart, Product product) {
-        for (OrderLineItem lineItem : cart) {
-            if (lineItem.getProduct().getID() == product.getID()) {
-                return lineItem;
-            }
-        }
-
-        return null;
     }
 }
